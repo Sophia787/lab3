534e3ca4822167d9c8c7525a499a125f9ded79fb
Sophia787 <91783576+Sophia787@users.noreply.github.com>
2021-11-06 16:46:43 +0300

    Delete nums.py

--- nums.py
+++
@@ -1,8 +0,0 @@
-def find_missing_nums(nums):
-    nums1 = list(range(1,len(nums)))
-    return list(set(nums1) - set(nums))
-
-while True:
-    print("Вход (введите значения списка через пробел): ")
-    nums = [int(i) for i in input('nums = ').split()]
-    print("Вывод: ", find_missing_nums(nums))
